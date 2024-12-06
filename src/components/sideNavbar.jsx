831034e8c56705cbf53d260e55f2b87ec994a265
Aiden Montgomery <Aidenmontg@icloud.com>
2024-12-06 06:06:36 -0500

    Fixed transition error

--- src/components/sideNavbar.jsx
+++ src/components/sideNavbar.jsx
@@ -12,16 +12,18 @@ const SideNavbar = () => {
     const Menus = [
         {title: "Dashboard", icon: <RiDashboardFill />, link: "/"},
         {title: "Example1", icon: <BsBoxSeam />, link: "/example1"},
-        {title: "Example2", icon: <BsBracesAsterisk />, link: "example2",  spacing: true},
+        {title: "Example2", icon: <BsBracesAsterisk />, link: "/example2",  spacing: true},
     ]
 
     return (
         //Make the Side Box
-        <div className={`${BGColor} h-screen p-5 pt-8 ${open ? "w-52" : "w-20"  } duration-300 w-1/6 relative`}>
+        <div className={`${BGColor} h-screen p-5 pt-8 ${open ? "w-52" : "w-20"  } duration-300 w-1/6 left-0 relative`}>
             <BsArrowLeftShort className={`bg-white text-gray-800 text-3xl rounded-full absolute -right-3 top-9 border border-gray-800 cursor-pointer duration-300 ${open && "rotate-180"}`} onClick={() => setOpen(!open)} />
 
             <div className={"inline-flex"}>
-                <BsBezier2 className={`${BGColor} text-white text-4xl cursor-pointer block float-left ${open && "rotate-[360deg] duration-500"}`} />
+                <Link to="/">
+                    <BsBezier2 className={`${BGColor} text-white text-4xl cursor-pointer block float-left ${open && "rotate-[360deg] duration-700"} ${!open && "rotate-[-360deg] duration-700"}`} />
+                </Link>
                 <h1 className={` origin-left font-medium text-2xl text-white duration-300 ${!open && "scale-0"} `}>PlaceHolder</h1>
             </div>
 
