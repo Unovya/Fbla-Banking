4f6a404f9a54406787d5ea910f7b2ceb564a4ffe
Aiden Montgomery <Aidenmontg@icloud.com>
2024-12-07 14:49:24 -0500

    Changed structure

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import SideNavbar from "../components/sideNavbar.jsx";
 
+
+
 export default function HomePage() {
     return (
         <div className="flex  bg-gray-100">
             <SideNavbar/>
-            <div className="text-black">PlaceHolder</div>
+            <div className='container pl-8 pt-[1.9rem]'>
+                <div className="text-black text-3xl font-normal flex mb-4 items-center justify-between">
+                    <h3>Dashboard</h3>
+                </div>
+            </div>
+
+
         </div>
     );
 }
