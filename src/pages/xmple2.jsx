809cf2ec7b4426ec04b15baf5e34edd0f2779f85
Aiden Montgomery <Aidenmontg@icloud.com>
2024-12-08 01:39:02 -0500

    Added Admin Bal changer

--- src/pages/xmple2.jsx
+++ src/pages/xmple2.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import SideNavbar from "../components/sideNavbar.jsx";
+import {BalTest} from "../scripts/database-Stuff/DBComponents/balTest.jsx";
 
 export default function Xmple2() {
     return (
@@ -9,13 +10,19 @@ export default function Xmple2() {
                 <SideNavbar/>
             </div>
             <div
-            className="grid h-screen items-center grid-col-1 text-2xl w-full text-center align justify-center bg-white">
+                className="grid h-screen items-center grid-col-1 text-2xl w-full text-center align justify-center bg-white">
 
 
                 <div
                     className="block justify-center text-center p-6 w-80 h-40 items-center bg-black border border-gray-200 rounded-lg shadow hover:bg-darkGray dark:bg-grey-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <Link to='/example2' className="mb-2 text-2xl font-bold tracking-tight text-white">2nd page</Link>
+                    <p className="mb-2 text-2xl font-bold tracking-tight text-white">2nd page</p>
                     <Link to="/" className="text-blue-500 hover:underline"> Go back to home</Link>
+                </div>
+
+                <div
+                    className="block justify-center text-center p-6 w-80 h-48 items-center bg-black border border-gray-200 rounded-lg shadow hover:bg-darkGray dark:bg-grey-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <p className="mb-2 text-2xl font-bold tracking-tight text-white">Admin Bal update</p>
+                    <BalTest />
                 </div>
             </div>
         </>
