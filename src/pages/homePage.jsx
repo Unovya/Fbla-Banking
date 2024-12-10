f1d9400dbbfbfbdfc8f33b1593a6c193c2cd8422
Aiden Montgomery <Aidenmontg@icloud.com>
2024-12-09 22:43:43 -0500

    Added Prop cards

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SideNavbar from "../components/sideNavbar.jsx";
 import { HiOutlinePaperClip } from "react-icons/hi2";
 import {BalWidget} from "../scripts/database-Stuff/DBComponents/balWidget.jsx";
+import {GrMoney} from "react-icons/gr";
 
 
 
@@ -10,7 +11,7 @@ export default function HomePage() {
         <div className="flex  bg-gray-100">
             <SideNavbar/>
             {/* Main Container*/}
-            <div className='container pl-8 pt-[1.9rem]'>
+            <div className='container ml-12 pt-[1.9rem]'>
                 {/* Dashboard and report Gen container */}
                 <div className="text-black text-3xl font-normal flex mb-4 items-center justify-between">
                     <h3 className='flex'>Dashboard</h3>
@@ -21,8 +22,45 @@ export default function HomePage() {
                 </div>
                 {/* End */}
 
-                {/* Balance Card */}
-                <BalWidget />
+                {/* Top Cards */}
+                <div className='flex space-x-28'>
+                    {/* Balance Card */}
+                    <BalWidget/>
+                    {/* Card 2 (Will Specify later */}
+                    <div className="max-w-xs flex flex-col bg-white border border-t-4 border-t-green-600 shadow-sm rounded-xl mt-12">
+                        <div className="p-4 md:p-5">
+                            <h3 className="text-lg font-bold text-gray-800 ">PlaceHolder</h3>
+                            <div className='text-center flex'>
+                                <p className='font-bold items-center text-center'>PlaceHolder</p>
+                                <GrMoney className='text-3xl ml-40'/>
+                            </div>
+                        </div>
+                    </div>
+
+                {/* Card 3 (Will Specify later */}
+                    <div className="max-w-xs flex flex-col bg-white border border-t-4 border-t-green-600 shadow-sm rounded-xl mt-12">
+                        <div className="p-4 md:p-5">
+                            <h3 className="text-lg font-bold text-gray-800 ">PlaceHolder 2</h3>
+                            <div className='text-center flex'>
+                                <p className='font-bold items-center text-center'>PlaceHolder 2</p>
+                                <GrMoney className='text-3xl ml-40'/>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card 4 (Will Specify later */}
+                    <div className="max-w-xs flex flex-col bg-white border border-t-4 border-t-green-600 shadow-sm rounded-xl mt-12">
+                        <div className="p-4 md:p-5">
+                            <h3 className="text-lg font-bold text-gray-800 ">PlaceHolder 3</h3>
+                            <div className='text-center flex'>
+                                <p className='font-bold items-center text-center'>PlaceHolder 3</p>
+                                <GrMoney className='text-3xl ml-40'/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* End */}
+
+
                 {/* End */}
             </div>
             {/* End */}
