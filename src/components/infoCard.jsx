04350ac0aa7669f0840f482181f0025a87c64c81
Aiden Montgomery <Aidenmontg@icloud.com>
2024-12-10 15:44:40 -0500

    created card component for homepage

---
+++ src/components/infoCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function InfoCard({ title, content, borderColor, icon }) {
+    return (
+        <div
+            className={`w-full sm:w-80 md:w-96 flex flex-col bg-white border border-t-4 ${borderColor} shadow-sm rounded-xl mt-6`}>
+            <div className="p-4 md:p-5">
+                <h3 className="text-lg font-bold text-gray-800">{title}</h3>
+                <div className="text-center flex items-center justify-between">
+                    <p className="font-bold">{content}</p>
+                    {icon}
+                </div>
+            </div>
+        </div>
+    );
+}
