7ba562ecbd4cf1212b8e9afa84c18b8c817a58c8
Aiden Montgomery <Aidenmontg@icloud.com>
2024-12-12 22:47:11 -0500

    Added

---
+++ src/components/outPie.jsx
@@ -0,0 +1,26 @@
+import {Pie} from 'react-chartjs-2'
+import {Chart as ChartJS, Tooltip, Legend, ArcElement} from "chart.js";
+import React from 'react'
+ChartJS.register(Tooltip, Legend, ArcElement);
+
+
+
+export const PieChart = () =>{
+    const options = {}
+    const data = {
+        labels: ['Entertainment', 'Food', 'Mandatory'],
+        datasets: [
+            {
+                label: 'money spent',
+                data:[100, 50, 20],
+                backgroundColor: [
+                    "rgba(255,99,132,0.7)",
+                    "rgba(54,162,235,0.7)",
+                    "rgba(34,84,135,0.7)"
+                ],
+                hoverOffset: 4,
+            }
+        ]
+    }
+    return <Pie options={options} data={data} />
+}
