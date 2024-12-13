860e49432d58fa512778bc5b1e30329bab16035c
Aiden Montgomery <Aidenmontg@icloud.com>
2024-12-12 22:48:02 -0500

    Added

---
+++ src/scripts/database-Stuff/DBComponents/transTestWid.jsx
@@ -0,0 +1,20 @@
+import {useLiveQuery} from "dexie-react-hooks";
+import {db} from "../db.js";
+import React from "react";
+
+export function TransWidget() {
+    const transactions = useLiveQuery(() => db.transactionLog.toArray());
+
+    return (
+        <ul className='space-y-9'>
+            {transactions?.map((transaction) => (
+                <li key={transaction.id} className='flex items-center text-center text-xl content-center'>
+                    <p className='font-bold items-center text-white text-center'>name: {transaction.name}
+                        <br/> action: {transaction.action}
+                        <br/> amount: ${transaction.amount} <br/>category: {transaction.category}
+                        <br/> date: {transaction.date}</p>
+                </li>
+            ))}
+        </ul>
+    );
+}
