1b79826151ebc941e6833f562de4e1614bded782
Aiden Montgomery <Aidenmontg@icloud.com>
2024-12-14 11:48:42 -0500

    Increased Readability

--- src/scripts/database-Stuff/DBComponents/transTestWid.jsx
+++ src/scripts/database-Stuff/DBComponents/transTestWid.jsx
@@ -11,8 +11,10 @@ export function TransWidget() {
                 <li key={transaction.id} className='flex items-center text-center text-xl content-center'>
                     <p className='font-bold items-center text-white text-center'>name: {transaction.name}
                         <br/> action: {transaction.action}
-                        <br/> amount: ${transaction.amount} <br/>category: {transaction.category}
-                        <br/> date: {transaction.date}</p>
+                        <br/> amount: ${transaction.amount}
+                        <br/> category: {transaction.category}
+                        <br/> date: {transaction.date}
+                        <br/> time: {transaction.time}</p>
                 </li>
             ))}
         </ul>
