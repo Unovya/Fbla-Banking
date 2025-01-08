e664d724f280094500530d60e70d30ba5f203028
Aiden Montgomery <Aidenmontg@icloud.com>
2025-01-08 14:04:25 -0500

    Added new container for transactions

--- src/pages/transactionsPage.jsx
+++ src/pages/transactionsPage.jsx
@@ -9,7 +9,6 @@ import {TransWidget} from "../components/DBComponents/transTestWid.jsx";
 
 export default function TransactionsPage() {
 
-    const [filterOpen, setFilterOpen] = React.useState(false)
 
 
 
@@ -17,26 +16,19 @@ export default function TransactionsPage() {
         <div className="flex overflow-x-hidden w-screen h-screen overflow-y-hidden bg-gray-100">
             <SideNavbar />
             {/* Main Container */}
-            <div className="w-full ml-12 pt-[1.9rem]">
-                {/* Title and add Filter container */}
-                <div className="text-black ml-10 text-3xl font-normal flex mb-4 items-center space-x-10">
-                    <h3 className="flex">Transactions</h3>
-                    <button className="bg-gray-800 hover:bg-gray-700 active:bg-gray-600 text-base text-white font-semibold py-1 px-5 mr-4 mt-2 border border-gray-400 flex rounded-2xl shadow items-center duration-100" onClick={() => setFilterOpen(!filterOpen)}>
-                         Add Filter
-                    </button>
+            <div className="w-screen ml-[5.5rem] pt-[1.9rem]">
+                {/* Title container */}
+                <div className="text-black text-3xl font-normal flex mb-4 items-center space-x-10">
+                    <h3 className="flex">My Transactions</h3>
                 </div>
                 {/* Filter container */}
-                {/* Add Actual Filter thing later*/}
-                <div className={`text-black ml-10 text-lg ${!filterOpen && "hidden"} font-normal flex mb-4 items-center space-x-10`}>test</div>
-
-                {/* End */}
 
                 {/* Transactions scroll area*/}
-                <div className='flex flex-col  rounded-xl w-fit'>
-                    <div
-                        className=' h-[90vh]  overflow-y-auto p-4 md:p-5 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300'>
-                        <TransWidget />
-                    </div>
+                <div className=" bg-gray-800 text-3xl align-text-top font-normal flex mb-[-20px]  pb-[30px] mt-20 items-center w-11/12 h-14 rounded-lg ">
+                    <p className='text-white ml-5 mt-2 text-[19px]'>My Transactions</p>
+                </div>
+                <div className='flex flex-col bg-white rounded-xl h-72 w-11/12'>
+
                 </div>
             </div>
         </div>
