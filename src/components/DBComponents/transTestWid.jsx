ac6c4c4f2cb6ee6eb8a143e66e53e06b8fa945c5
Aiden Montgomery <Aidenmontg@icloud.com>
2025-01-10 21:40:49 -0500

    worked on overlay

--- src/components/DBComponents/transTestWid.jsx
+++ src/components/DBComponents/transTestWid.jsx
@@ -19,7 +19,7 @@ export function TransWidget() {
 
     const [TransAction, setTransAction] = React.useState('');
     const [TransName, setTransName] = React.useState('');
-    const [Transcategory, setTranscategory] = React.useState('');
+    const [TransCategory, setTransCategory] = React.useState('');
     const [TransDate, setTransDate] = React.useState('');
     const [TransId, setTransId] = React.useState('');
     const [Popup, setOpenPopup] = React.useState(false);
@@ -28,52 +28,76 @@ export function TransWidget() {
         setTransId(id);
         setTransName(name);
         setTransDate(date);
-        setTranscategory(category);
+        setTransCategory(category);
         setTransAction(action);
     }
+
     function openPopup(name, action, category, date, id){
         setupPopup(name, action, category, date, id);
         setOpenPopup(!Popup);
-
     }
+
+    const closePopup = () => {
+        setOpenPopup(false)
+    }
+
+
 
     return (
         <>
-            <div className='flex flex-col m-5 bg-gray-100 drop-shadow rounded-xl h-[90%] w-[69%]'>
-                <div className='flex flex-row bg-white h-6 ml-3 mr-3 mt-3 mb-1 rounded font-bold shadow'>
-                    <p className='ml-3 w-[55px] text-left'>ID #</p>
-                    <p className='w-[150px] text-left'>Name</p>
-                    <p className='w-[140px] text-left'>Category</p>
-                    <p className='w-[100px] text-left'>Action</p>
-                    <p className='w-[100px] text-left'> Amount</p>
-                    <p className=''>Date</p>
-                </div>
-
-
-                <ul>
-                    {[...(transactions ?? [])].reverse().map((transaction) => (
-                        <li key={transaction.id} className='last:mb-3'>
-                            <button type='button'
-                                    className={`flex flex-row ${colorSwitch(transaction.id)} text-black h-6 ml-3 mr-3 mt-2 rounded font-bold shadow`} onClick={() => openPopup(transaction.name, transaction.action, transaction.category, transaction.date, transaction.id)}>
-                                <p className='ml-3 w-[55px] text-left'>{transaction.id}</p>
-                                <p className='w-[150px] text-left'>{transaction.name.charAt(0).toUpperCase() + transaction.name.slice(1)}</p>
-                                <p className='w-[140px] text-left'>{transaction.category.charAt(0).toUpperCase() + transaction.category.slice(1)}</p>
-                                <p className='w-[100px] text-left'>{transaction.action.charAt(0).toUpperCase() + transaction.action.slice(1)}</p>
-                                <p className='w-[100px] text-left'>{transaction.action === 'withdraw' && `-$${shorten(transaction.amount, 6)}`} {transaction.action === 'deposit' && `+$${shorten(transaction.amount, 6)}`}</p>
-                                <p className='text-left'>{transaction.date}</p>
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-
-            </div>
-            {Popup && <div className='fixed bg-white/80 mt-10 top-[205px] left-0 bottom-0 right-0 w-2/6 h-2/6 mx-auto backdrop-blur rounded-xl'>
-                    <div className='m-auto '>
-
+            <div className={`flex flex-col m-5 bg-gray-100 overflow-x-hidden overflow-y-auto drop-shadow rounded-xl h-[90%] w-[69%] ${Popup ? 'backdrop-blur-sm' : ''}`}>
+                <div className='flex flex-col m-5 bg-gray-100 overflow-x-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 rounded-xl h-[90%] w-[95%]'>
+                    <div className='flex flex-row bg-white h-6 ml-3 mr-3 mt-3 mb-1 rounded w-[98%] font-bold shadow'>
+                        <p className='ml-3 w-[55px] text-left'>ID #</p>
+                        <p className='w-[150px] text-left'>Name</p>
+                        <p className='w-[140px] text-left'>Category</p>
+                        <p className='w-[100px] text-left'>Action</p>
+                        <p className='w-[100px] text-left'> Amount</p>
+                        <p className=''>Date</p>
                     </div>
-                </div>}
 
-</>
-)
-;
+
+                    <ul>
+                        {[...(transactions ?? [])].reverse().map((transaction) => (
+                            <li key={transaction.id} className='last:mb-3'>
+                                <button type='button'
+                                        className={`flex flex-row ${colorSwitch(transaction.id)} text-black h-6 ml-3 mr-3 w-[98%] mt-2 rounded font-bold shadow`} onClick={() => openPopup(transaction.name, transaction.action, transaction.category, transaction.date, transaction.id)}>
+                                    <p className='ml-3 w-[55px] text-left'>{transaction.id}</p>
+                                    <p className='w-[150px] text-left'>{transaction.name.charAt(0).toUpperCase() + transaction.name.slice(1)}</p>
+                                    <p className='w-[140px] text-left'>{transaction.category.charAt(0).toUpperCase() + transaction.category.slice(1)}</p>
+                                    <p className='w-[100px] text-left'>{transaction.action.charAt(0).toUpperCase() + transaction.action.slice(1)}</p>
+                                    <p className='w-[100px] text-left'>{transaction.action === 'withdraw' && `-$${shorten(transaction.amount, 6)}`} {transaction.action === 'deposit' && `+$${shorten(transaction.amount, 6)}`}</p>
+                                    <p className='text-left'>{transaction.date}</p>
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+
+                </div>
+            </div>
+            {Popup && <div className='fixed inset-0 bg-black/50 flex justify-center items-center backdrop-blur-sm' onClick={closePopup}>
+                <div
+                    className={`fixed bg-gray-200 mt-10 top-[205px] left-0 bottom-0 right-0 w-[25%] justify-center items-center h-fit mx-auto backdrop-blur rounded-xl`} onClick={(e) => e.stopPropagation()}>
+                    <div className='m-auto text-2xl ml-auto mr-auto w-fit font-semibold'>
+                        <div className=''>
+                            <hr className="h-2 my-1 bg-white border w-full rounded"/>
+                            <button
+                                className='float-right text-red-700 font-medium justify-center items-center text-center'
+                                onClick={() => closePopup()}>X
+                            </button>
+                            <p className='font-bold justify-center items-center text-center'>#{TransId}</p>
+                        </div>
+                        <p className=''>Name of Transaction: {TransName}</p>
+                        <p>Category: {TransCategory}</p>
+                        <p>Action: {TransAction}</p>
+                        <p>Date of Transaction: {TransDate}</p>
+                        <hr className="h-2 my-1 bg-white border w-full rounded"/>
+                    </div>
+                </div>
+            </div>
+            }
+
+        </>
+    )
+        ;
 }
--- src/components/transactions.jsx
+++ src/components/transactions.jsx
@@ -9,7 +9,7 @@ const Transactions = () => {
                 className=" bg-gray-800 text-3xl align-text-top font-normal flex mb-[-20px]  pb-[30px] mt-20 items-center w-11/12 h-14 rounded-lg ">
                 <p className='text-white ml-5 mt-2 text-[19px]'>My Transactions</p>
             </div>
-            <div className='flex flex-col bg-white rounded-xl h-96 shadow w-11/12'>
+            <div className='flex flex-col bg-white rounded-xl h-[80%] shadow w-11/12'>
                 <TransWidget />
             </div>
         </>
