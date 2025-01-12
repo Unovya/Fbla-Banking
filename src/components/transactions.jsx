7d5d02ed6bede464599c17a5617560da354f3706
Aiden Montgomery <Aidenmontg@icloud.com>
2025-01-12 16:49:55 -0500

    added 'AddTransactions'

--- src/components/transactions.jsx
+++ src/components/transactions.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {TransWidget} from "./DBComponents/transTestWid.jsx";
+import {AddTransactions} from "./addTransactions.jsx"
 
 
 const Transactions = () => {
@@ -9,8 +10,9 @@ const Transactions = () => {
                 className=" bg-gray-800 text-3xl align-text-top font-normal flex mb-[-20px]  pb-[30px] mt-20 items-center w-11/12 h-14 rounded-lg ">
                 <p className='text-white ml-5 mt-2 text-[19px]'>My Transactions</p>
             </div>
-            <div className='flex flex-col bg-white rounded-xl h-[80%] shadow w-11/12'>
+            <div className='flex flex-row bg-white rounded-xl h-[80%] shadow w-11/12'>
                 <TransWidget />
+                <AddTransactions />
             </div>
         </>
 )
