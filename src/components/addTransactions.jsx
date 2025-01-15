35c56e22330d1a362eab421fe51ccef5de96eb0f
Aiden Montgomery <Aidenmontg@icloud.com>
2025-01-15 11:16:55 -0500

    Test Div

--- src/components/addTransactions.jsx
+++ src/components/addTransactions.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 export const AddTransactions = () => {
     return (
-        <div className='flex flex-col mt-5 ml-5 mb-5 bg-gray-100 overflow-x-hidden overflow-y-hidden rounded-xl h-[30%] w-[27%]'></div>
-    );
-};
+            <div className='flex flex-col bg-gray-100 overflow-x-hidden overflow-y-hidden rounded-xl  items-center'>
+                <h1 className='mt-3 mx-3 text-black font-bold w-fit'>Test</h1>
+            </div>
+            );
+            };
 
