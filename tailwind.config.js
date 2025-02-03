3eab88bf14dcbb7e13ebbfe9c045f5a2156c2d77
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-02 20:36:59 -0500

    made fade-in

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': {opacity: 0},
+          '100%': {opacity: 1},
+
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn 1.5s'
+      }
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
