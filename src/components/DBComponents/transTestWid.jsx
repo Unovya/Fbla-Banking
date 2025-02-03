98c662187de4824eaac3cf1d5608d22264d530d9
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-02 20:35:32 -0500

    added hovering and sizing

--- src/components/DBComponents/transTestWid.jsx
+++ src/components/DBComponents/transTestWid.jsx
@@ -4,8 +4,8 @@ import React from "react";
 import {AddTransactions} from "../addTransactions.jsx"
 
 function colorSwitch(num) {
-    if (num % 2 === 0) return "bg-gray-200";
-    else return "bg-white";
+    if (num % 2 === 0) return "bg-gray-200 hover:bg-gray-300";
+    else return "bg-white hover:bg-gray-100";
 }
 function shorten(num, maxLength) {
     if (num.length > maxLength) {
@@ -23,25 +23,14 @@ export function TransWidget() {
     const [TransDate, setTransDate] = React.useState('');
     const [TransId, setTransId] = React.useState('');
     const [TransAmount, setTransAmount] = React.useState(0);
-    const [Popup, setOpenPopup] = React.useState(false);
 
-    function setupPopup(name, action, category, amount, date, id) {
+    function setupDetails(name, action, category, amount, date, id) {
         setTransId(id);
         setTransName(name);
         setTransDate(date);
         setTransCategory(category);
         setTransAction(action);
         setTransAmount(amount);
-        console.log(id)
-    }
-
-    function openPopup(name, action, category, amount, date, id){
-        setupPopup(name, action, category, amount, date, id);
-        setOpenPopup(!Popup);
-    }
-
-    const closePopup = () => {
-        setOpenPopup(false)
     }
 
 
@@ -50,7 +39,7 @@ export function TransWidget() {
         <>
             {/* Blurring div and gray area */}
             <div
-                className={`flex flex-row ml-3 mt-5 mb-5 mr-1 bg-gray-100 overflow-x-hidden overflow-y-auto drop-shadow rounded-xl h-[95%] w-[67%] ${Popup ? 'backdrop-blur-sm' : ''}`}>
+                className={`flex flex-row ml-3 mt-5 mb-5 mr-1 bg-gray-100 overflow-x-hidden overflow-y-auto drop-shadow rounded-xl h-[95%] w-[67%]}`}>
                 {/* Scroll Area */}
                 <div
                     className='flex flex-col m-5 bg-gray-100 overflow-x-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 rounded-xl h-[90%] w-[95%]'>
@@ -70,7 +59,7 @@ export function TransWidget() {
                             <li key={transaction.id} className='last:mb-3'>
                                 <button type='button'
                                         className={`flex flex-row ${colorSwitch(transaction.id)} text-black h-6 ml-3 mr-3 w-[98%] mt-2 rounded font-bold shadow`}
-                                        onClick={() => openPopup(transaction.name, transaction.action, transaction.category, transaction.amount, transaction.date, transaction.id)}>
+                                        onClick={() => setupDetails(transaction.name, transaction.action, transaction.category, transaction.amount, transaction.date, transaction.id)}>
                                     <p className='ml-3 w-[55px] text-left'>{transaction.id}</p>
                                     <p className='w-[150px] text-left'>{transaction.name.charAt(0).toUpperCase() + transaction.name.slice(1)}</p>
                                     <p className='w-[140px] text-left'>{transaction.category.charAt(0).toUpperCase() + transaction.category.slice(1)}</p>
@@ -85,10 +74,11 @@ export function TransWidget() {
                 </div>
             </div>
 
-            <div className='flex flex-col h-[90%] w-[30%] mt-5 ml-2'>
-                <div className='flex flex-col bg-gray-100 mb-5 overflow-x-hidden overflow-y-hidden rounded-xl h-[31%] items-center'>
+            <div className='flex flex-col h-[85%] w-[30%] mt-5 ml-2'>
+                <div
+                    className='flex flex-col bg-gray-100 mb-5 overflow-x-hidden overflow-y-hidden rounded-xl h-[31%] items-center'>
                     <h1 className='mt-3 mx-3 text-black font-bold w-fit'>Transaction Details</h1>
-                    <p>Transaction Id: #{TransName !== ''  && TransId }</p>
+                    <p className='flex flex-row'>Transaction Id: #{TransId}</p>
                     <p>Name of Transaction: {TransName.charAt(0).toUpperCase() + TransName.slice(1)}</p>
                     <p>Category: {TransCategory.charAt(0).toUpperCase() + TransCategory.slice(1)}</p>
                     <p>Action: {TransAction.charAt(0).toUpperCase() + TransAction.slice(1)}</p>
@@ -96,7 +86,7 @@ export function TransWidget() {
                     <p>Date of Transaction: {TransDate.charAt(0).toUpperCase() + TransDate.slice(1)}</p>
                 </div>
 
-                <AddTransactions />
+                <AddTransactions/>
             </div>
         </>
     )
