11b06e8945d8bf66540c2b499b6e9fdc61e75171
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-08 10:15:02 -0500

    added outside </>

--- src/components/DashCard.jsx
+++ src/components/DashCard.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 export function DashCard({ title, content, borderColor, icon }) {
     return (
-        <div
-            className={`w-96 sm:w-72 md:w-80 flex flex-col bg-white border border-t-4 ${borderColor} shadow-sm rounded-xl mt-6`}>
-            <div className="p-4 md:p-5">
-                <h3 className="text-lg font-bold text-gray-800">{title}</h3>
-                <div className="text-center flex items-center justify-between">
-                    <p className="font-bold">{content}</p>
-                    {icon}
+        <>
+            <div
+                className={`w-96 sm:w-72 md:w-80 flex flex-col bg-white border border-t-4 ${borderColor} shadow-sm rounded-xl mt-6`}>
+                <div className="p-4 md:p-5">
+                    <h3 className="text-lg font-bold text-gray-800">{title}</h3>
+                    <div className="text-center flex items-center justify-between">
+                        <p className="font-bold">{content}</p>
+                        {icon}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
