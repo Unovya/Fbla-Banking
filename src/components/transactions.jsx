edec416e101a3d95dddfd7b4d7b149bc923af41c
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-14 07:48:26 -0500

    Update transactions.jsx

--- src/components/transactions.jsx
+++ src/components/transactions.jsx
@@ -6,11 +6,11 @@ const Transactions = () => {
     return (
         <>
             <div
-                className=" bg-gray-800 text-3xl flex flex-row align-text-top font-normal flex mb-[-20px]  pb-[30px] mt-9 items-center w-[94%] h-14 rounded-lg ">
+                className=" bg-gray-800 text-3xl flex flex-row align-text-top font-normal mb-[-20px]  pb-[30px] mt-9 items-center w-[95%] h-14 rounded-lg ">
                 <p className='text-white ml-5 mt-2 text-[19px]'>My Transactions</p>
                 <p className="text-white ml-44 mt-2 text-[19px]">Click on a Transaction to View its Details</p>
             </div>
-            <div className='flex flex-row bg-white rounded-xl h-[85%] shadow w-[94%]'>
+            <div className='flex flex-row bg-white rounded-xl h-[85%] shadow w-[95%]'>
                 <TransWidget />
             </div>
         </>
