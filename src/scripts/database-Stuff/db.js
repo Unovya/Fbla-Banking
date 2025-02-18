47064743950501ae173c8ff2496d0383d9f7e100
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-17 23:26:13 -0500

    Removed Autoincrementation from transactions

--- src/scripts/database-Stuff/db.js
+++ src/scripts/database-Stuff/db.js
@@ -3,6 +3,6 @@ import Dexie from "dexie";
 export const db = new Dexie('UnovianDB');
 db.version(1).stores({
     currentBal: '++id, balance',
-    transactionLog: '++id, name, action, amount, category, date, time',
+    transactionLog: 'id, name, action, amount, category, date, time',
     reoccurringCosts: '++id, name, amount, date'
 });
