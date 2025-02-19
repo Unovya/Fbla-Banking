37f3eb8c8fdf4a86c05ffb4768b983fc8265e5cd
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-18 23:15:15 -0500

    On app First Run, Now automatically initializes a balance!!!

--- src/scripts/database-Stuff/db.js
+++ src/scripts/database-Stuff/db.js
@@ -6,3 +6,30 @@ db.version(1).stores({
     transactionLog: 'id, name, action, amount, category, date, time',
     reoccurringCosts: '++id, name, amount, date'
 });
+
+db.open().catch((error) => {
+    console.error('Failed to open DB: ', error);
+});
+
+
+const isFirstRun = localStorage.getItem('isFirstRun') === null;
+
+if (isFirstRun){
+    console.log('First time running app, initializing DB');
+
+    async function initializeDB() {
+        try {
+            await db.currentBal.add({ balance: 0 });
+
+            console.log('DB initialized');
+
+            // Set the flag so this doesn't run again
+            localStorage.setItem('isFirstRun', 'false');
+        } catch (error) {
+            console.error('Error initializing DB: ', error);
+        }
+    }
+    initializeDB();
+}else {
+    console.log('App opened before, no initialization needed.');
+}
