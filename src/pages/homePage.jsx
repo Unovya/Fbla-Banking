1bf500de767400aa2560be752fe33feb089e3876
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-19 21:31:30 -0500

    changed spacing on dashcards and added help Button

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -6,6 +6,8 @@ import { GrMoney } from "react-icons/gr";
 import {DashCard} from "../components/DashCard.jsx";
 import {OutPie} from "../components/graphs/outPie.jsx";
 import {IncomeLine} from "../components/graphs/incomeLine.jsx";
+import CSVDownloader from "../components/generateCSV.jsx";
+import HelpButton from "../components/helpButton.jsx";
 
 
 export default function HomePage() {
@@ -17,25 +19,24 @@ export default function HomePage() {
                 {/* Dashboard and report Gen container */}
                 <div className="text-black text-3xl font-normal flex mb-4 items-center justify-between">
                     <h3 className="flex">Dashboard</h3>
-                    <button className="bg-gray-800 hover:bg-gray-700 active:bg-gray-600 text-base text-white font-semibold py-1 px-2 mr-4 mt-2 border border-gray-400 flex rounded-lg shadow items-center duration-100">
-                        <HiOutlinePaperClip /> Generate Report
-                    </button>
+                    <HelpButton content={'Test'}  />
+                    <CSVDownloader />
                 </div>
                 {/* End */}
 
                 {/* Top Cards container */}
-                <div className="flex flex-wrap justify-start space-x-5 gap-5">
+                <div className="flex flex-wrap justify-start grid-cols-4 space-x-5 gap-5">
                     {/* Top Cards */}
-                    <div className="w-80 sm:w-64 md:w-72">
+                    <div className="w-fit">
                         <DashCard title="Balance" content=<BalWidget/> borderColor="border-green-600" icon=<GrMoney className='text-3xl float-right'/>/>
                     </div>
-                    <div className="w-80 sm:w-64 md:w-72">
+                    <div className="w-fit">
                         <DashCard title="PlaceHolder 1" content="PlaceHolder 1" borderColor="border-violet-600" icon=<GrMoney className='text-3xl float-right'/>/>
                     </div>
-                    <div className="w-80 sm:w-64 md:w-72">
+                    <div className="w-fit">
                         <DashCard title="PlaceHolder 2" content="PlaceHolder 2" borderColor="border-red-600" icon=<GrMoney className='text-3xl float-right'/>/>
                     </div>
-                    <div className="w-80 sm:w-64 md:w-72">
+                    <div className="w-fit">
                         <DashCard title="PlaceHolder 3" content="PlaceHolder 3" borderColor="border-blue-600" icon=<GrMoney className='text-3xl float-right'/>/>
                     </div>
                 </div>
