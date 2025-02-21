26f1b48b8039e3e688b2abdc274ac5428b606d4e
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-20 20:47:57 -0500

    added option to add classNames to button

--- src/components/helpButton.jsx
+++ src/components/helpButton.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
-export default function HelpButton({content, extraContent = null}) {
+export default function HelpButton({content, extraContent = null, className = ""}) {
     const [isOpen, setIsOpen] = useState(false);
     const helpRef = useRef(null);
 
@@ -21,19 +21,14 @@ export default function HelpButton({content, extraContent = null}) {
     return (
         <div className="relative flex flex-col mt-5" ref={helpRef}>
             <button
-                className="px-3 bg-white text-gray-700 border border-black rounded-3xl transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-black hover:bg-violet-500 hover:text-white"
+                className={`px-3 bg-white text-gray-700 ${className} border border-black rounded-3xl transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-black hover:bg-violet-500 hover:text-white`}
                 onClick={() => setIsOpen(!isOpen)}
             >
                 ?
             </button>
             {isOpen && (
                 <>
-                    <motion.div
-                        initial={{ opacity: 0, y: -5 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -5 }}
-                        className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 p-3 bg-gray-800 text-white rounded-lg shadow-lg w-64 text-sm z-50"
-                    >
+                    <motion.div initial={{ opacity: 0, y: -5 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -5 }} className="absolute h-fit text-wrap top-full left-1/2 transform text-[14px]  -translate-x-1/2 mt-2 p-3 bg-gray-800 text-white rounded-lg shadow-lg w-80 z-50">
                         {content}
                     </motion.div>
                     {extraContent}
