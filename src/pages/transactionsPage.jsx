123566bc9798b1a6916402032705aab7c1d5bd1f
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-20 22:20:06 -0500

    commented out help button to keep words for onboarding

--- src/pages/transactionsPage.jsx
+++ src/pages/transactionsPage.jsx
@@ -6,8 +6,8 @@ import Transactions from "../components/transactions.jsx";
 
 
 
-export default function TransactionsPage() {
 
+export default function TransactionsPage() {
 
 
 
@@ -17,8 +17,19 @@ export default function TransactionsPage() {
             {/* Main Container */}
             <div className="w-screen ml-[2.5rem] pt-[1.9rem]">
                 {/* Title container */}
-                <div className="text-black text-3xl font-normal flex mb-4 items-center space-x-10">
+                <div className="text-black text-3xl font-normal  grid grid-cols-2 mb-4 items-center">
                     <h3 className="flex">My Transactions</h3>
+
+                    <div className="pr-10 w-fit">
+                        {/*
+                        <HelpButton
+                            content={
+                            "Welcome To Your Transactions! Here you can add, delete, and view your transactions! " +
+                            "If you want to filter your Transactions all you have to do is press where it says 'ID # Name ...' Right above your transactions!" +
+                            "You can also delete any transactions right below the transactions table! "
+                        } />
+                        */}
+                    </div>
                 </div>
                 {/* Transactions area*/}
                 <Transactions />
