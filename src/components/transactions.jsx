1b090c4904c27f29f14d17db99f06f56db08a102
Aiden Montgomery <Aidenmontg@icloud.com>
2025-02-24 07:45:13 -0500

    Sizing changes

--- src/components/transactions.jsx
+++ src/components/transactions.jsx
@@ -6,11 +6,11 @@ const Transactions = () => {
     return (
         <>
             <div
-                className=" bg-violet-800 text-3xl flex flex-row align-text-top font-normal mb-[-20px]  pb-[30px] mt-9 items-center w-[95%] h-14 rounded-lg ">
+                className=" bg-violet-800 text-3xl flex flex-row align-text-top font-normal mb-[-20px]  pb-[30px] mt-9 items-center w-[97%] h-14 rounded-lg ">
                 <p className='text-white ml-5 mt-2 text-[19px]'>My Transactions</p>
                 <p className="text-white ml-44 mt-2 text-[19px]">Click on a Transaction to View its Details</p>
             </div>
-            <div className='flex flex-row bg-white rounded-xl h-[85%] shadow w-[95%]'>
+            <div className='flex flex-row bg-white rounded-xl h-[85%] shadow w-[97%]'>
                 <TransWidget />
             </div>
         </>
